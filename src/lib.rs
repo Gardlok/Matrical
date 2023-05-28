9c1bbbb0ac83bd2e7137950e059c99f9617a937c
Anthony Gardner <59458181+Gardlok@users.noreply.github.com>
2023-05-28 22:38:16 +0000

    Progress on DB API

--- src/lib.rs
+++ src/lib.rs
@@ -22,78 +22,50 @@ use surrealdb::Session;
 use surrealdb::Value;
 use surrealdb::Error;
 
+use std::collections::HashMap;
 
-
-// Modify the DependencyInjectionContainer struct to include the strategies and parameterized_queries fields:
-
-struct DependencyInjectionContainer {
-    strategies: Vec<Box<dyn Strategy>>,
-    parameterized_queries: Vec<ParameterizedQuery>,
-}
-// Implement the Strategy trait and its related traits (FilterStrategy, SortStrategy, AggregateStrategy) as shown in your code:
-
-//
+// Strategy trait
+// Defines a set of methods that can be used to perform various operations on a given data set.
 trait Strategy {
     fn prepare(&self, data: &HashMap<String, String>) -> Result<(), String>;
     fn execute(&self, data: &HashMap<String, String>) -> Result<(), String>;
     fn result(&self) -> Result<(), String>;
 }
-// 
+
+// FilterStrategy trait
+// Defines a set of methods that can be used to filter a given data set.
 trait FilterStrategy: Strategy {
     fn filter(&self, data: &HashMap<String, String>) -> Result<(), String>;
 }
 
+// SortStrategy trait
+// Defines a set of methods that can be used to sort a given data set.
 trait SortStrategy: Strategy {
     fn sort(&self, data: &HashMap<String, String>) -> Result<(), String>;
 }
 
+// AggregateStrategy trait
+// Defines a set of methods that can be used to aggregate a given data set.
 trait AggregateStrategy: Strategy {
     fn aggregate(&self, data: &HashMap<String, String>) -> Result<(), String>;
 }
-//Update the DependencyInjectionContainer implementation to include methods for adding strategies and parameterized queries:
 
-impl DependencyInjectionContainer {
-    // ...
-
-    fn add_strategy(&mut self, strategy: Box<dyn Strategy>) {
-        self.strategies.push(strategy);
-    }
-
-    fn add_parameterized_query(&mut self, query: ParameterizedQuery) {
-        self.parameterized_queries.push(query);
-    }
-}
-// Modify the SurrealDBAdapter implementation to use the dependency injection container and execute the strategies accordingly:
-
-impl SurrealDBAdapter {
-    // ...
-
-
-}
-// Use the execute_strategies method within your matrix operation methods to apply the desired strategies:
-
-pub fn get_matrix_value(&self, row: usize, col: usize) -> Result<bool, MatricalError> {
-    // Get data from SurrealDB or other source
-    let data = HashMap::new();  // Placeholder, TODO: replace with actual data retrieval
-
-    // Apply strategies
-    self.execute_strategies(&data)?;
-
-
-    Ok(value)
+// ParameterizedQuery struct
+// Defines a parameterized query that can be used to perform various operations on a given data set.
+struct ParameterizedQuery {
+    query: String,
+    parameters: Vec<String>,
 }
 
-pub fn set_matrix_value(&mut self, row: usize, col: usize, value: bool) -> Result<(), MatricalError> {
-    // Get data from SurrealDB or other source
-    let data = HashMap::new();  // Placeholder, replace with actual data retrieval
-
-    // Apply strategies
-    self.execute_strategies(&data)?;
-
-
-    Ok(())
+// DependencyInjectionContainer struct
+// Defines a container for strategies and parameterized queries that can be used to perform various operations on a given data set.
+struct DependencyInjectionContainer {
+    strategies: Vec<Box<dyn Strategy>>,
+    parameterized_queries: Vec<ParameterizedQuery>,
 }
-/////////////////////////////////////////////////////////
+
+// SurrealDBAdapter struct
+// Defines an adapter for the SurrealDB database that can be used to execute strategies and parameterized queries.
 pub struct SurrealDBAdapter {
     datastore: Datastore,
     session: Session,
@@ -101,41 +73,33 @@ pub struct SurrealDBAdapter {
 }
 
 impl SurrealDBAdapter {
-    pub async fn new() -> Result<Self, MatricalError> {
-        let datastore = Datastore::new("memory").await?;
+
+    pub fn new() -> Result<Self, Error> {
+        let datastore = Datastore::new("memory")?;
         let session = Session::for_kv().with_ns("test").with_db("test");
-        Ok(SurrealDBAdapter {
+        let di_container = DependencyInjectionContainer::new();
+        Ok(Self {
             datastore,
             session,
+            di_container,
         })
     }
 
-    pub async fn run_query(&self, sql: &str) -> Result<Vec<Value>, MatricalError> {
-        let result = self.datastore.execute(sql, &self.session, None, false).await?;
-        Ok(result.into_iter().map(|res| res.result).collect())
+	// add_strategy()
+	// Adds a strategy to the Dependency Injection Container.
+    pub fn add_strategy(&mut self, strategy: Box<dyn Strategy>) {
+        self.di_container.strategies.push(strategy);
     }
 
-    pub fn get_matrix_value(&self, row: usize, col: usize) -> Result<bool, MatricalError> {
-        let sql = format!("SELECT matrix[{}][{}] AS value FROM matrical;", row, col);
-        let result = self.run_query(&sql)?;
-        if let Some(value) = result.first() {
-            if let Value::Bool(b) = value {
-                Ok(*b)
-            } else {
-                Err(MatricalError::ValueError)
-            }
-        } else {
-            Err(MatricalError::NotFoundError)
-        }
+	// add_parameterized_query()
+	// Adds a parameterized query to the Dependency Injection Container.
+    pub fn add_parameterized_query(&mut self, query: ParameterizedQuery) {
+        self.di_container.parameterized_queries.push(query);
     }
 
-    pub fn set_matrix_value(&mut self, row: usize, col: usize, value: bool) -> Result<(), MatricalError> {
-        let sql = format!("UPDATE matrical SET matrix[{}][{}] = {};", row, col, value);
-        self.run_query(&sql)?;
-        Ok(())
-    }
-
-    pub fn execute_strategies(&self, data: &HashMap<String, String>) -> Result<(), String> {
+	// execute_strategies()
+	// Executes the strategies in the Dependency Injection Container on the given data set.
+    pub async fn execute_strategies(&self, data: &HashMap<String, String>) -> Result<(), String> {
         for strategy in &self.di_container.strategies {
             strategy.prepare(data)?;
             strategy.execute(data)?;
@@ -143,8 +107,77 @@ impl SurrealDBAdapter {
         }
         Ok(())
     }
+
+	// get_matrix_value()
+	// Retrieves a matrix value from the given data set.
+    pub fn get_matrix_value(&self, row: usize, col: usize) -> Result<bool, MatricalError> {
+        // Get data from SurrealDB or other source
+        let data = HashMap::new();  // Placeholder, replace with actual data retrieval
+
+        // Apply strategies
+        self.execute_strategies(&data)?;
+
+        // Perform the matrix value retrieval operation
+
+        Ok(value)
+    }
+
+	// set_matrix_value()
+	// Sets a matrix value in the given data set.
+    pub fn set_matrix_value(&mut self, row: usize, col: usize, value: bool) -> Result<(), MatricalError> {
+        // Get data from SurrealDB or other source
+        let data = HashMap::new();  // Placeholder, replace with actual data retrieval
+
+        // Apply strategies
+        self.execute_strategies(&data)?;
+
+        // Perform the matrix value setting operation
+        // ...
+
+        Ok(())
+    }
 }
 
+impl DependencyInjectionContainer {
+    fn new() -> Self {
+        Self {
+            strategies: Vec::new(),
+            parameterized_queries: Vec::new(),
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
+// TEMPLATE Access to SurrealDB
+impl Strategy for YourStrategyType {
+    fn prepare(&self, data: &HashMap<String, String>) -> Result<(), String> {
+        // Implement prepare method for your strategy
+    }
+
+    fn execute(&self, data: &HashMap<String, String>) -> Result<(), String> {
+        // Implement execute method for your strategy
+    }
+
+    fn result(&self) -> Result<(), String> {
+        // Implement result method for your strategy
+    }
+}
+
+impl FilterStrategy for YourFilterStrategyType {
+    fn filter(&self, data: &HashMap<String, String>) -> Result<(), String> {
+        // Implement filter method for your filter strategy
+    }
+}
+impl SortStrategy for YourSortStrategyType {
+    fn sort(&self, data: &HashMap<String, String>) -> Result<(), String> {
+        // Implement sort method for your sort strategy
+    }
+}
+impl AggregateStrategy for YourAggregateStrategyType {
+    fn aggregate(&self, data: &HashMap<String, String>) -> Result<(), String> {
+        // Implement aggregate method for your aggregate strategy
+    }
+}
 
 
 
@@ -204,8 +237,6 @@ pub trait MatrixOperation {
 }
 
 
-
-
 pub fn get_matrix_value(&self, row: usize, col: usize) -> Result<bool, MatricalError> {
     // Get data from SurrealDB or other source
     let data = HashMap::new();  // Placeholder, TODO: replace with actual data retrieval
@@ -221,9 +252,6 @@ pub fn set_matrix_value(&mut self, row: usize, col: usize, value: bool) -> Resul
     self.execute_strategies(&data)?;
     Ok(())
 }
-
-
-
 
 
 // Element Operation
@@ -260,120 +288,33 @@ impl ViewOperation {
     }
 }
 
-// impl MatrixStrategy for ViewOperation {
-//     fn execute(
-//         &self,
-//         matrix: &Matrix,
-//         _index: Option<(usize, usize)>,
-//         _other: Option<bool>,
-//     ) -> Result<(), MatricalError> {
-//         // Check if the coordinates are within the matrix dimensions
-//         if self.top_left.0 >= matrix.data.dim().0
-//             || self.top_left.1 >= matrix.data.dim().1
-//             || self.bottom_right.0 >= matrix.data.dim().0
-//             || self.bottom_right.1 >= matrix.data.dim().1
-//         {
-//             return Err(MatricalError::IndexOutOfBounds);
-//         }
+impl MatrixStrategy for ViewOperation {
+    fn execute(
+        &self,
+        matrix: &Matrix,
+        _index: Option<(usize, usize)>,
+        _other: Option<bool>,
+    ) -> Result<(), MatricalError> {
+        // Check if the coordinates are within the matrix dimensions
+        if self.top_left.0 >= matrix.data.dim().0
+            || self.top_left.1 >= matrix.data.dim().1
+            || self.bottom_right.0 >= matrix.data.dim().0
+            || self.bottom_right.1 >= matrix.data.dim().1
+        {
+            return Err(MatricalError::IndexOutOfBounds);
+        }
 
-//         // Iterate over the sub-matrix and print the values
-//         for i in self.top_left.0..=self.bottom_right.0 {
-//             for j in self.top_left.1..=self.bottom_right.1 {
-//                 let value = matrix.data[(i, j)].load();
-//                 println!("Value at ({}, {}): {}", i, j, value);
-//             }
-//         }
-//         Ok(())
-//     }
-// }
+        // Iterate over the sub-matrix and print the values
+        for i in self.top_left.0..=self.bottom_right.0 {
+            for j in self.top_left.1..=self.bottom_right.1 {
+                let value = matrix.data[(i, j)].load();
+                println!("Value at ({}, {}): {}", i, j, value);
+            }
+        }
+        Ok(())
+    }
+}
 
-// // Set the state
-// pub struct StateSetOperation;
-// //
-// impl MatrixOperation for StateSetOperation {
-//     fn execute(&self, context: &MatrixContext, value: AtomicBool) -> Result<(), MatricalError> {
-//         // Here we'll set all the states to true
-//         for row in &mut context.matrix {
-//             for element in row {
-//                 element.state.atomic_bool.store(value);
-//             }
-//         }
-//         Ok(())
-//     }
-// }
-
-// pub struct StateGetOperation;
-
-// impl MatrixOperation for StateGetOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-        
-//     }
-// }
-
-// pub struct StateToggleOperation;
-
-// impl MatrixOperation for StateToggleOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct BitwiseAndOperation;
-
-// impl MatrixOperation for BitwiseAndOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct BitwiseOrOperation;
-
-// impl MatrixOperation for BitwiseOrOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct BitwiseXorOperation;
-
-// impl MatrixOperation for BitwiseXorOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct BitwiseNotOperation;
-
-// impl MatrixOperation for BitwiseNotOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct CreateAttributeOperation;
-
-// impl MatrixOperation for CreateAttributeOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct GetAttributeOperation;
-
-// impl MatrixOperation for GetAttributeOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
-
-// pub struct RemoveAttributeOperation;
-
-// impl MatrixOperation for RemoveAttributeOperation {
-//     fn execute(&self, context: &MatrixContext) -> Result<(), MatricalError> {
-//         // Implement the MatrixOperation trait
-//     }
-// }
 
 // The Matrix struct now holds a Box<dyn MatrixOperation> which allows for changing the operation at runtime
 impl<V> Matrix {
@@ -385,10 +326,6 @@ impl<V> Matrix {
         self._context.operation.execute(&self._context)
     }
 }
-
-
-
-
 
 // Functors are functions that operate on the matrix
 // Execute a function on the value of a Element
@@ -412,68 +349,6 @@ where
 
 }
 
-// pub fn from_enum_matrix(matrix: Array2<MatrixElement>) -> Self {
-//     let rows = matrix.nrows();
-//     let cols = matrix.ncols();
-//     let mut data = Vec::with_capacity(rows * cols);
-//     for element in matrix.iter() {
-//         let val = match element {
-//             MatrixElement::VariantA => true,  // or whatever logic you want
-//             MatrixElement::VariantB => false, // or whatever logic you want
-//             MatrixElement::VariantC => true,  // or whatever logic you want
-//             // ...
-//         };
-//         data.push(AtomicBool::new(val));
-//     }
-//     AttributeMatrix {
-//         matrix: ArcSwap::new(Arc::new(Array2::from_shape_vec((rows, cols), data).unwrap())),
-//     }
-// }
-
-// // example of a method for updating the matrix using parallel processing
-// pub fn parallel_update<F>(&self, update_func: F)
-// where
-//     F: Fn(usize, usize) -> bool + Sync,
-// {
-//     let rows = self.matrix.load().rows();
-//     let cols = self.matrix.load().cols();
-//     (0..rows).into_par_iter().for_each(|row| {
-//         for col in 0..cols {
-//             let new_val = update_func(row, col);
-//             self.update_value(row, col, new_val);
-//         }
-//     });
-// }
-
-// mod Mask {
-//     //////////////////////////////////////////////////
-//     // DASHER
-//     use dashmap::DashMap;
-//     type MaskId = String;
-
-//     // Our concurrent mask storage.
-//     struct MaskStorage {
-//         masks: DashMap<MaskId, AttributeMatrix>,
-//     }
-//     impl MaskStorage {
-//         fn new() -> Self {
-//             MaskStorage {
-//                 masks: DashMap::new(),
-//             }
-//         }
-//         fn insert(&self, mask_id: MaskId, mask: AttributeMatrix) {
-//             self.masks.insert(mask_id, mask);
-//         }
-//         fn get(&self, mask_id: &MaskId) -> Option<AttributeMatrix> {
-//             self.masks.get(mask_id).map(|mask_ref| mask_ref.clone())
-//         }
-//         fn remove(&self, mask_id: &MaskId) -> Option<AttributeMatrix> {
-//             self.masks.remove(mask_id).map(|(_, mask)| mask)
-//         }
-//     }
-// }
-
-//     // The error type returned by operations
 
 // // Error handling
 pub enum MatricalError {
@@ -525,81 +400,32 @@ struct ErrorHandler;
 use std::fmt::Debug;
 
 // Assuming you have an Error type
-#[derive(Debug)]
-pub struct Error {
-    message: String,
-    // you can add more fields here
-}
+// #[derive(Debug)]
+// pub struct Error {
+//     message: String,
+//     // you can add more fields here
+// }
 
-impl Error {
-    pub fn new(message: &str) -> Self {
-        Error {
-            message: message.to_string(),
-        }
-    }
-}
+// impl Error {
+//     pub fn new(message: &str) -> Self {
+//         Error {
+//             message: message.to_string(),
+//         }
+//     }
+// }
 
-// Handler trait definition TODO: Implement this
-pub trait Handler<T> {
-    fn call(&self, context: &Context<T>);
-}
+// // Handler trait definition TODO: Implement this
+// pub trait Handler<T> {
+//     fn call(&self, context: &Context<T>);
+// }
 
-// An ErrorHandler
-pub struct ErrorHandler;
+// // An ErrorHandler
+// pub struct ErrorHandler;
 
-impl Handler<Error> for ErrorHandler {
-    fn call(&self, context: &Context<Error>) {
-        // Here you can handle the error in any way you want
-        println!("Error occurred in '{}': {:?}", context.name, context.data);
-    }
-}
+// impl Handler<Error> for ErrorHandler {
+//     fn call(&self, context: &Context<Error>) {
+//         // Here you can handle the error in any way you want
+//         println!("Error occurred in '{}': {:?}", context.name, context.data);
+//     }
+// }
 
-use surrealdb::Datastore;
-use surrealdb::Session;
-use surrealdb::Value;
-use surrealdb::Error;
-
-pub struct SurrealDBAdapter {
-    datastore: Datastore,
-    session: Session,
-}
-
-impl SurrealDBAdapter {
-    pub async fn new() -> Result<Self, Error> {
-        let datastore = Datastore::new("memory").await?;
-        let session = Session::for_kv().with_ns("test").with_db("test");
-        Ok(SurrealDBAdapter {
-            datastore,
-            session,
-        })
-    }
-
-    pub async fn run_query(&self, sql: &str) -> Result<Vec<Value>, Error> {
-        let result = self.datastore.execute(sql, &self.session, None, false).await?;
-        Ok(result.into_iter().map(|res| res.result).collect())
-    }
-
-    pub fn get_matrix_value(&self, row: usize, col: usize) -> Result<bool, MatricalError> {
-        let sql = format!("SELECT matrix[{}][{}] AS value FROM matrical;", row, col);
-        let result = self.run_query(&sql)?;
-        if let Some(value) = result.first() {
-            if let Value::Bool(b) = value {
-                Ok(*b)
-            } else {
-                Err(MatricalError::ValueError)
-            }
-        } else {
-            Err(MatricalError::NotFoundError)
-        }
-    }
-
-    pub fn set_matrix_value(&mut self, row: usize, col: usize, value: bool) -> Result<(), MatricalError> {
-        let sql = format!("UPDATE matrical SET matrix[{}][{}] = {};", row, col, value);
-        self.run_query(&sql)?;
-        Ok(())
-    }
-
-    // Implement the remaining matrix operations methods
-    // ...
-
-}
